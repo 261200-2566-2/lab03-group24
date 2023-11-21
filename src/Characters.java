c4f36d1ac86b4a145e17fafcd2ff772680968671
tent <tteenntt.2003@hotmail.com>
2023-11-21 15:50:39 +0700

    delete private name not use

--- src/Characters.java
+++ src/Characters.java
@@ -2,21 +2,13 @@ public class Characters {
     private int hp;
     private int level;
     private int mana;
-    private String name;
 
-    private void _Characters (String _name , int _hp, int _level , int _mana){
+    private void _Characters ( int _hp, int _level , int _mana){
         this.hp = _hp;
         this.level = _level;
         this.mana = _mana;
-        this.name = _name;
 
-    }
-    public void rename (String name){
-        this.name = name;
-    }
 
-    public String getName() {
-        return name;
     }
 
 }
