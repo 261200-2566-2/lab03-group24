44439a3b1916c6227af1611904f0a06d13986787
tent <tteenntt.2003@hotmail.com>
2023-11-21 14:50:29 +0700

    template RPG แยก class ออกมาให้ดูดีย์

--- src/RPG.java
+++ src/RPG.java
@@ -3,12 +3,19 @@ public class RPG {
     private int level;
     private int mana;
     private String name;
-    public void characters (int _level , int _hp , int _mana , String _name){
+    private boolean sword, shield;
+
+    private void characters (int _level , int _hp , int _mana , String _name){
         this.hp = _hp;
         this.mana = _mana;
         this.level = _level;
         this.name = _name;
+
     }
+
+//    private int calculatehp(int dmgtaken){
+//
+//    }
 
 
 }
